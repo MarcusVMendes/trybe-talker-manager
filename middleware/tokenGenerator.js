55d4ce50523fa275af96f2770f5bf4f3ae82563f
MarcusVMendes <marcus.mendes89@gmail.com>
2021-12-29 18:35:41 -0300

    Requisito 3 concluido

--- middleware/tokenGenerator.js
+++ middleware/tokenGenerator.js
@@ -3,7 +3,7 @@ const getRandomString = () => Math.random().toString(16).substr(2);
 module.exports = (_req, res) => {
   const token = () => (getRandomString() + getRandomString()).substr(0, 16);
 
-  return res.status(200).json(token());
+  return res.status(200).json({ token: token() });
 };
 
 /*
